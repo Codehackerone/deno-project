aea8deb813393c80e4cd86b91ba0cdfe4b25254e
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-02 00:22:41 +0530

    intial commit

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { Application,Router,RouterContext } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+
+const app=new Application();
+const router=new Router();
+
+router.get("/",(ctx: RouterContext)=>
+{
+    ctx.response.body='Hello World';
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener('listen',({hostname,port,secure})=>
+{
+    console.log(`Listening on ${secure ? 'https://':'http://'}${hostname || 'localhost'}:${port}`);
+    
+});
+
+await app.listen({port:8001});
