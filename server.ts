6a82b8531894013f92890f5fbf081c2937d0cdbe
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-02 00:29:43 +0530

    added dependency file

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Application,Router,RouterContext } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+import { Application,Router,RouterContext } from "./deps.ts";
 
 const app=new Application();
 const router=new Router();
