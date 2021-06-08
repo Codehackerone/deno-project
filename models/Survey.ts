8f9b3430d460e7571cc16d23004dbc343534f163
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-08 08:37:54 +0530

    Survey model updated

--- models/Survey.ts
+++ models/Survey.ts
@@ -1,13 +1,19 @@
 import { surveysCollection } from "../mongo.ts";
+import BaseModel from "./BaseModel.ts";
 
-export default class Survey{
+export default class Survey extends BaseModel{
     public id:string="";
 
     constructor(
         public userId:string,
         public name:string,
         public description:string
-    ){}
+    ){
+        super();
+        this.userId = userId;
+        this.name = name;
+        this.description = description;
+    }
 
     async create(this:any){
         delete this.id;
@@ -17,13 +23,24 @@ export default class Survey{
         return this;
     }
 
-    static async findByUser(userId:string):Promise<any>{
+    static async findByUser(userId:string):Promise<Survey[]>{
         let surveys= surveysCollection.find({userId},{noCursorTimeout:false} as any);
-        return surveys.toArray();
+        return surveys.map((survey: any) => Survey.prepare(survey));
     }
 
     static async findById(_id:string):Promise<any>{
         let survey=surveysCollection.findOne({_id},{noCursorTimeout:false} as any);
         return survey;
     }
+
+    static prepare(data: any): Survey {
+        data = BaseModel.prepare(data);
+        const survey = new Survey(
+          data.userId,
+          data.name,
+          data.description,
+        );
+        survey.id = data.id;
+        return survey;
+      }
 }
