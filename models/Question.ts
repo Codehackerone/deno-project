1c1709ccaa816a884db2d3ad7206ff52d673e0dc
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-10 10:51:58 +0530

    question model added

---
+++ models/Question.ts
@@ -0,0 +1,15 @@
+import { questionCollection } from "../mongo.ts";
+import BaseModel from "./BaseModel.ts";
+
+export default class Question extends BaseModel {
+  public id: string = "";
+  constructor(
+    public surveyId: string,
+    public text: string,
+    public type: QuestionType,
+    public required: boolean,
+    public data: any,
+  ) {
+    super();
+  }
+}
--- mongo.ts
+++ mongo.ts
@@ -21,4 +21,4 @@ await client.connect({
 const db = client.database("deno_project");
 export const usersCollection = db.collection("users");
 export const surveysCollection = db.collection("survey");
-export const QuestionController=db.collection("question");
+export const questionCollection=db.collection("question");
